7ed4aca5997284eb00ad4815ed55746b9f0e642c
Ben Coburn <ben@roonlabs.com>
2021-11-12 15:42:04 -0500

    reformat windows output to match mac/linux, use wnetenumresources to list shares on remote host

--- src/roon_watcher.c
+++ src/roon_watcher.c
@@ -7,6 +7,8 @@
 #define WIN32_LEAN_AND_MEAN
 #include <winsock2.h>
 #include <windows.h>
+#include <winnetwk.h>
+#include <lm.h>
 
 #else
 
@@ -22,9 +24,10 @@
 #endif
 
 #include "bdsm.h"
+#ifndef PLATFORM_WINDOWS
 #include "smb_session.h"
 #include "netbios_session.h"
-#ifndef PLATFORM_WINDOWS
+
 #include "smb2/smb2.h"
 #include "smb2/libsmb2.h"
 #include "smb2/libsmb2-raw.h"
@@ -71,6 +74,122 @@ static void print_entry(const char *what,
             netbios_ns_entry_name(entry),
             netbios_ns_entry_type(entry)); fflush(stdout);
 }
+
+#ifdef PLATFORM_WINDOWS
+static int nt_error_to_roon_error(int nt_error) {
+    switch(nt_error) {
+    case ERROR_ACCESS_DENIED:
+    case ERROR_INVALID_PASSWORD:
+        return ROON_SMB_UNAUTHORIZED;
+    case ERROR_BAD_NET_NAME:
+    case ERROR_NO_NET_OR_BAD_PATH:
+        return ROON_SMB_NOT_FOUND;
+    case ERROR_EXTENDED_ERROR:
+    case ERROR_NO_NETWORK:
+        return ROON_SMB_NETWORK_ERROR;
+    default:
+        return ROON_SMB_UNEXPECTED_ERROR;
+    }
+}
+
+static int list_shares_smb1(void *p_opaque,
+                            char *name,
+                            uint32_t ip) {
+    struct watcher_options *options = (struct watcher_options *)p_opaque;
+    struct in_addr  addr;
+    DWORD nt_error;
+    print_if((options->mode == MODE_TEST), "list_shares_smb1 for server: %s, ip: %x\n", name, ip);
+
+    char *server_name;
+    if (ip != 0) {
+        addr.S_un.S_addr = ip;
+        asprintf(&server_name, "\\\\%s", inet_ntoa(addr));
+    } else {
+        asprintf(&server_name, "\\\\%s", name);
+    }
+
+    LPNETRESOURCE server_netresource = calloc(1, sizeof(NETRESOURCE));
+    server_netresource->dwType = RESOURCETYPE_DISK;
+    server_netresource->dwUsage = RESOURCEUSAGE_CONTAINER;
+    server_netresource->lpLocalName = NULL;
+    server_netresource->lpRemoteName = server_name;
+    server_netresource->lpProvider = NULL;
+
+    nt_error = WNetUseConnection(NULL, server_netresource, options->password, options->username, 0, NULL, NULL, NULL);
+    if (nt_error == NO_ERROR) {
+        print_if((options->mode == MODE_TEST), "    connected to %s as %s / %s\n", server_name, options->username, options->password);
+
+        //server_netresource->dwType = RESOURCETYPE_ANY;
+        DWORD share_ret;
+        HANDLE enum_handle;
+        DWORD buf_size = 16384;     // 16K is a good size
+        DWORD num_entries = -1;        // enumerate all possible entries
+        LPNETRESOURCE share_netresource;    // pointer to enumerated structures
+        DWORD i;
+
+        nt_error = WNetOpenEnum(RESOURCE_GLOBALNET,
+                                RESOURCETYPE_ANY,
+                                0,
+                                server_netresource,
+                                &enum_handle);
+
+        if (nt_error != NO_ERROR) {
+            print_if((options->mode == MODE_TEST),"WnetOpenEnum failed with error %d\n", nt_error);
+            return nt_error_to_roon_error(nt_error);
+        }
+
+        share_netresource = (LPNETRESOURCE) GlobalAlloc(GPTR, buf_size);
+        if (share_netresource == NULL) {
+            print_if((options->mode == MODE_TEST), "WnetOpenEnum failed with error %d\n", nt_error);
+            return ROON_SMB_UNEXPECTED_ERROR;;
+        }
+
+        do {
+            ZeroMemory(share_netresource, buf_size);
+            share_ret = WNetEnumResource(enum_handle,  // resource handle
+                                            &num_entries,      // defined locally as -1
+                                            share_netresource,      // LPNETRESOURCE
+                                            &buf_size);     // buffer size
+            if (share_ret == NO_ERROR) {
+                print_if((options->mode == MODE_TEST), "        share count: %i\n", num_entries);
+                char *format = "        share name: %-20s %-20s";
+                char *prefix = "     ";
+                if (options->mode == MODE_SHARES) {
+                    format = "%s";
+                    prefix = " type:";
+                }
+                for (i = 0; i < num_entries; i++) {
+                    printf(format, share_netresource[i].lpRemoteName,
+                           share_netresource[i].lpComment);
+                    if (share_netresource[i].dwType == RESOURCETYPE_DISK) {
+                        fprintf(stdout, "%sDISKTREE", prefix);
+                    }
+                    if (share_netresource[i].dwType == RESOURCETYPE_PRINT) {
+                        fprintf(stdout, "%sPRINTQ", prefix);
+                    }
+                    fprintf(stdout, "\n"); fflush(stdout);
+                }
+            } else if (share_ret != ERROR_NO_MORE_ITEMS) {
+                print_if((options->mode == MODE_TEST),"WNetEnumResource failed with error %d\n", share_ret);
+                break;
+            }
+        } while (share_ret != ERROR_NO_MORE_ITEMS);
+ 
+        GlobalFree((HGLOBAL) share_netresource);
+        nt_error = WNetCloseEnum(enum_handle);
+
+        if (nt_error != NO_ERROR) {
+            print_if((options->mode == MODE_TEST),"WNetCloseEnum failed with error %d\n", nt_error);
+            return nt_error_to_roon_error(nt_error);
+        }
+    } else {
+        print_if((options->mode == MODE_TEST), "    WNetUseConnection failed to connect to %s as %s / %s, error code: %d\n", server_name, options->username, options->password, nt_error);
+        return nt_error_to_roon_error(nt_error);
+    }
+
+    free(server_netresource);
+}
+#endif
 
 #ifndef PLATFORM_WINDOWS
 static int get_nb_state(smb_session *session) {
@@ -283,19 +402,23 @@ static int list_shares_smb2(void *p_opaque,
         
     return ROON_SMB_NETWORK_ERROR;
 }
+#endif
 
 static int list_shares(watcher_options *options,
                        char* name,
                        uint32_t ip) {
     bool test_mode = (options->mode == MODE_TEST);
-    int smb1_ret = ROON_SMB_SUCCESS;
-    int smb2_ret = ROON_SMB_SUCCESS;
 
+    print_if(test_mode, "list_shares_smb for server: %s, ip: %x\n", name, ip);
+
+    int smb1_ret = ROON_SMB_SUCCESS;
+#ifndef PLATFORM_WINDOWS
+    int smb2_ret = ROON_SMB_SUCCESS;
     print_if(test_mode, "  attempting to list shares over smb2 with credentials\n");
     smb2_ret = list_shares_smb2(options, name);
     print_if(test_mode, "  return value: %d\n", smb2_ret);
     if ((smb2_ret == ROON_SMB_SUCCESS) && (options->mode == MODE_SHARES)) return ROON_SMB_SUCCESS;
-
+#endif
     print_if(test_mode, "  attempting to list shares over smb1 with credentials\n");
     smb1_ret = list_shares_smb1(options, name, ip);
     print_if(test_mode, "  return value: %d\n", smb1_ret);
@@ -307,17 +430,17 @@ static int list_shares(watcher_options *options,
         guest_creds->workgroup = "";
         guest_creds->username  = "Guest";
         guest_creds->password  = "password";
-
+#ifndef PLATFORM_WINDOWS
         print_if(test_mode, "  attempting to list shares over smb2 as guest\n");
         smb2_ret = list_shares_smb2(guest_creds, name);
         print_if(test_mode, "  return value: %d\n", smb2_ret);
         if (smb2_ret == ROON_SMB_SUCCESS) return ROON_SMB_SUCCESS;
-        
+#endif
         print_if(test_mode, "  attempting to list shares over smb1 as guest\n");
         smb1_ret = list_shares_smb1(guest_creds, name, ip);
         print_if(test_mode, "  return value: %d\n", smb1_ret);
     }
-
+#ifndef PLATFORM_WINDOWS
     if (smb1_ret == smb2_ret) {
         return smb1_ret;
     } else if ((smb2_ret == ROON_SMB_NOT_FOUND) || (smb1_ret == ROON_SMB_NOT_FOUND)) {
@@ -327,8 +450,8 @@ static int list_shares(watcher_options *options,
     } else {
         return smb2_ret;
     }
-}
 #endif
+}
 
 static void on_entry_added(void *p_opaque,
                            netbios_ns_entry *entry) {
@@ -338,6 +461,10 @@ static void on_entry_added(void *p_opaque,
     if (options->mode == MODE_TEST) {
         int list_ret = list_shares(options, netbios_ns_entry_name(entry), netbios_ns_entry_ip(entry));
     }
+#else
+    if (options->mode == MODE_TEST) {
+        int list_ret = list_shares_smb1(options, netbios_ns_entry_name(entry), netbios_ns_entry_ip(entry));
+    }    
 #endif
 }
 
@@ -391,6 +518,10 @@ static int scan_hosts(watcher_options *options) {
 }
 
 static int run_test(watcher_options *options) {
+#ifdef PLATFORM_WINDOWS
+    //win_main();
+#endif
+    
     return scan_hosts(options);
 }
 
@@ -398,38 +529,35 @@ static int run_hosts(intmax_t freq, watcher_options *options) {
     return scan_hosts(options);
 }
 
-#ifndef PLATFORM_WINDOWS
 static int run_shares(char *name_type, char *name_or_ip, watcher_options *options) {
-    char* name;
-    uint32_t ip;
+    char* name = "";
+    uint32_t ip = 0;
     
     netbios_ns *ns;
     ns = netbios_ns_new();
-    
+
     if (strcmp(name_type, "IP") == 0) {
-//        struct in_addr addr;
-//        addr.s_addr = 
-//        int parse_ret = inet_aton(AF_INET, name_or_ip, &addr);
-//        ip = addr.s_addr;
         ip = inet_addr(name_or_ip);
+#ifndef PLATFORM_WINDOWS
         name = netbios_ns_inverse(ns, ip);
         if (name == NULL) {
             name = name_or_ip;
         }
+#endif
     } else if (strcmp(name_type, "NAME") == 0) {
         name = name_or_ip;
-        int fs_result = netbios_ns_resolve(ns, name_or_ip, NETBIOS_FILESERVER, &ip);
+#ifndef PLATFORM_WINDOWS
+        int fs_result = netbios_ns_resolve(ns, name, NETBIOS_FILESERVER, &ip);
         if (fs_result != 0) {
             fs_result == netbios_ns_resolve(ns, name_or_ip, NETBIOS_WORKSTATION, &ip);
             if (fs_result != 0) {
                 ip = 0;
             }
         }
-    }
-
+#endif
+    }    
     return list_shares(options, name, ip);
 }
-#endif
 
 static void set_credentials(int argc, char** argv, watcher_options *options) {
     int offset = 0;
@@ -470,14 +598,12 @@ int main(int argc, char** argv) {
         options->mode = MODE_HOSTS;
         intmax_t freq = 0;
         if (argc == 3) freq = strtoimax(argv[2], NULL, 10);
-        return run_hosts(freq, options);
-#ifndef PLATFORM_WINDOWS        
+        return run_hosts(freq, options);     
     } else if (strcmp(argv[1], "shares") == 0) {
         if ((argc < 4) || (argc > 7)) return usage();
         options->mode = MODE_SHARES;
         set_credentials(argc, argv, options);
         return run_shares(argv[2], argv[3], options);
-#endif
     } else {
         return usage();
     }
